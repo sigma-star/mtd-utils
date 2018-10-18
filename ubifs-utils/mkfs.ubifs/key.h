2fd902f11823433ba9332da3106253b674563542
Richard Weinberger <richard@nod.at>
2018-10-18 16:36:41 +0200

    mkfs.ubifs: Make r5 hash binary string aware
    As of now all filenames known by UBIFS are strings with a NUL
    terminator. With encrypted filenames a filename can be any binary
    string and the r5 function cannot search for the NUL terminator.
    UBIFS always knows how long a filename is, therefore we can change
    the hash function to iterate over the filename length to work
    correctly with binary strings.
    
    Signed-off-by: Richard Weinberger <richard@nod.at>
    Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

--- ubifs-utils/mkfs.ubifs/key.h
+++ ubifs-utils/mkfs.ubifs/key.h
@@ -64,9 +64,8 @@ static inline uint32_t key_r5_hash(const char *s, int len)
 {
 	uint32_t a = 0;
 	const signed char *str = (const signed char *)s;
-	(void)len;
 
-	while (*str) {
+	while (len--) {
 		a += *str << 4;
 		a += *str >> 4;
 		a *= 11;
