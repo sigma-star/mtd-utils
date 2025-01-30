sigma-star/mtd-utils

commit 78f4f38bd3de8b5c590cbc09ce43812306207d95

ubi-utils: Fix integer overflow in mtdinfo.c
Report of the static analyzer:
The value of an arithmetic expression 'reginfo->offset + i * reginfo->erasesize' is a subject to overflow
because its operands are not cast to a larger data type before performing arithmetic

Corrections explained:
Added casting i and start to unsigned long long

Triggers found by static analyzer Svace.

Signed-off-by: Anton Moryakov <ant.v.moryakov@gmail.com>
Reviewed-by: Zhihao Cheng <chengzhihao1@huawei.com>
Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

ubi-utils/mtdinfo.c
@@ -185,7 +185,7 @@ static void print_ubi_info(const struct mtd_info *mtd_info,
 static void print_region_map(const struct mtd_dev_info *mtd, int fd,
 			     const region_info_t *reginfo)
 {
-	unsigned long start;
+	unsigned long long start;
 	int i, width;
 	int ret_locked, errno_locked, ret_bad, errno_bad;
 
@@ -203,7 +203,7 @@ static void print_region_map(const struct mtd_dev_info *mtd, int fd,
 		ret_locked = ret_bad = errno_locked = errno_bad = 0;
 
 	for (i = 0; i < reginfo->numblocks; ++i) {
-		start = reginfo->offset + i * reginfo->erasesize;
+		start = reginfo->offset + (unsigned long long)i * reginfo->erasesize;
 		printf(" %*i: %08lx ", width, i, start);
 
 		if (ret_locked != -1) {

commit 53a16db8824ee93ab066e57744cb6f7ea5ba053d

ubi-utils: mtdinfo: Fix type truncation compiling warning for 'start' variable
Fix following compiling warning by using the right format:
 ubi-utils/mtdinfo.c:207:21: warning: format ‘%lx’ expects argument of
 type ‘long unsigned int’, but argument 4 has type ‘long long unsigned
 int’

Fixes: 78f4f38bd3de8 ("ubi-utils: Fix integer overflow in mtdinfo.c")
Signed-off-by: Zhihao Cheng <chengzhihao1@huawei.com>
Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

ubi-utils/mtdinfo.c
@@ -204,7 +204,7 @@ static void print_region_map(const struct mtd_dev_info *mtd, int fd,
 
 	for (i = 0; i < reginfo->numblocks; ++i) {
 		start = reginfo->offset + (unsigned long long)i * reginfo->erasesize;
-		printf(" %*i: %08lx ", width, i, start);
+		printf(" %*i: %08llx ", width, i, start);
 
 		if (ret_locked != -1) {
 			ret_locked = mtd_is_locked(mtd, fd, i);