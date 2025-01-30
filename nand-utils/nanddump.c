sigma-star/mtd-utils

commit c890094638880072cfa95a06cb1353f0214a11a0

nanddump: don't leak copied command line arguments
For some command line flags, the argument string is copied. Simply
writing over the buffer leads to a resource leak if the same flag
is specified on the command line more than once.

This patch adds a free() call to the old buffer before overwriting
it with the new copy.

Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

nand-utils/nanddump.c
@@ -162,6 +162,7 @@ static void process_options(int argc, char * const argv[])
 				start_addr = simple_strtoll(optarg, &error);
 				break;
 			case 'f':
+				free(dumpfile);
 				dumpfile = xstrdup(optarg);
 				break;
 			case 'l':

commit 9d755ba03f6947ed0f77df8a2bcdb1b6585dabf9

nand-utils: nanddump: Fix unexpected type compiling warning for argument 'dumpfile'
The const pointer argument cannot be passed into libc function 'free()',
otherwise the compiler will complain following message:
 nand-utils/nanddump.c:168:10: warning: passing argument 1 of ‘free’
 discards ‘const’ qualifier from pointer target type

Fixes: c89009463888 ("nanddump: don't leak copied command line arguments")
Signed-off-by: Zhihao Cheng <chengzhihao1@huawei.com>
Signed-off-by: David Oberhollenzer <david.oberhollenzer@sigma-star.at>

nand-utils/nanddump.c
@@ -102,6 +102,7 @@ static void process_options(int argc, char * const argv[])
 {
 	int error = 0;
 	bool oob_default = true;
+	char *dumpfile_tmp = NULL;
 
 	for (;;) {
 		int option_index = 0;
@@ -165,8 +166,8 @@ static void process_options(int argc, char * const argv[])
 				start_addr = simple_strtoll(optarg, &error);
 				break;
 			case 'f':
-				free(dumpfile);
-				dumpfile = xstrdup(optarg);
+				free(dumpfile_tmp);
+				dumpfile = dumpfile_tmp = xstrdup(optarg);
 				break;
 			case 'l':
 				length = simple_strtoll(optarg, &error);